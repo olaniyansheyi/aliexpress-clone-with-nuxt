e9995295cf8779b932ba664d76cdce88bd8ae7b1
sheymantech <olaniyansheyi1704@gmail.com>
2024-06-20 14:57:58 +0100

    initial commit

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div id="MainLayout" class="w-full fixed z-50">
+    <div
+      id="TopMenu"
+      class="w-full bg-[#FAFAFA] border-b md:block hidden"
+    ></div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayoutVue from "~/layouts/MainLayout.vue";
+</script>
+
+<template>
+  <MainLayoutVue>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      index page
+    </div>
+  </MainLayoutVue>
+</template>
